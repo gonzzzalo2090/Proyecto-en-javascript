aa847ca2e6b9171288401eb797780003b7182710
Gonzalo Nicolas Ojeda <95065115+gonzzzalo2090@users.noreply.github.com>
2022-05-21 00:49:48 -0300

    empezando proyecto

---
+++ Js/01-hola-mundo.js
@@ -0,0 +1,6 @@
+/*alert("Hola mundo con JS");
+alert("BIENVENIDO AL CURSO JS");*/
+//document.write ("Hola mundo en js");
+//console.log ("muestrame la consola");
+//console.log (88 + 4);
+
---
+++ Js/02-variables.js
@@ -0,0 +1,12 @@
+//Variables
+//Una variable es un contenedor de informacon
+
+var pais = "España";
+var continente = "Europa";
+var pais_y_continente = pais+' '+continente;
+
+var pais = ("MEXICO");
+var continente = ("america");
+
+console.log(pais,continente);
+alert (pais_y_continente);
